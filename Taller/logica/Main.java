0f4aa3150af812c59b509ba4d9c248ac736286b2
VicenteRojasMalhue <vicente.rojas03@alumnos.ucn.cl>
2026-03-23 19:41:22 -0400

    Primer commit para probar el uso de VS Code

--- Taller/logica/Main.java
+++ Taller/logica/Main.java
@@ -1,10 +1,12 @@
 package logica;
+// Vicente Ignacio Rojas Malhue / ICCI / 21.894.251-2
+//
 
 public class Main {
 	
 	public static void main(String[] args)	{
 		
-		System.out.println("hola mundo");
+		System.out.println("hola mundo, intento aprender o entender como hacer un commit con VS Code");
 	}
 
 }
