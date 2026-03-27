6b31094d85dbd785bcedd76e5716761a34b9de05
millazs12 <millaray.zepeda@alumnos.ucn.cl>
2026-03-26 21:23:16 -0300

    agragado de mas menus y for para usario

--- Taller/logica/Main.java
+++ Taller/logica/Main.java
@@ -70,10 +70,71 @@ public static void main(String[] args) throws FileNotFoundException {
 
             switch (numero) {
                 case 1:
-                    System.out.print("Usuario: ");
-                    String persona = opcion.next();
-                    System.out.print("Contraseña: ");
-                    String contrasena = opcion.next();
+                	int acceso = 0;
+                	String logrado = "";//se guarda el usuario
+
+                    while (acceso == 0) {
+                        System.out.print("Usuario: ");
+                        String persona = opcion.next();
+
+                        System.out.print("Contraseña: ");
+                        String contrasena = opcion.next();
+
+                        // FOR para buscar
+                        for (int k = 0; k < i; k++) {
+                            if (usuarios[k].equals(persona) && contrasenas[k].equals(contrasena)) {
+                                acceso = 1;
+                                logrado = usuarios[k];
+                                break;
+                            }
+                        }
+
+                        if (acceso == 1) {
+                        	int opcion1 = 0;
+                            System.out.println("Acceso permitido");
+                            System.out.println(" ");
+                            System.out.println("Bienvenido "+ logrado+ "!");
+                            System.out.println(" ");
+                            System.out.println("Que deseas realizar?\r\n"
+                            		+ "\r\n"
+                            		+ "1) Registrar actividad.\r\n"
+                            		+ "2) Modificar actividad.\r\n"
+                            		+ "3) Eliminar actividad.\r\n"
+                            		+ "4) Cambiar contraseña.\r\n"
+                            		+ "5) Salir.");
+                            while (opcion1 != 5) {
+                            	System.out.print("");
+                            	opcion1 = opcion.nextInt();
+                            	
+                            	if (opcion1 == 5) {
+                            		opcion1 = 3; //cierra todo
+                            		break;
+                                }
+                            	switch(opcion1) {
+                            	case 1:
+                            		System.out.println("111");
+                            		break;
+                            	case 2:
+                            		System.out.println("222");
+                            		break;
+                            	case 3:
+                            		System.out.println("333");
+                            		break;
+                            	case 4:
+                            		System.out.println("444");
+                            		break;
+                            	case 5:
+                                    System.out.println("Saliendo programa total");
+                                    break;
+               
+                            	default:
+                            		System.out.println("otra vez:");
+                            	}
+                            } 
+                        } else {
+                            System.out.println(" Usuario o contraseña incorrectos, intenta nuevamente");
+                        }
+                    }
                     break;
 
                 case 2:
@@ -95,22 +156,23 @@ public static void main(String[] args) throws FileNotFoundException {
                         
                         if (numero2 == 5) {
                             numero = 3; //cierra todo
+                            break;
                         }
                     	switch(numero2) {
                     	case 1:
-                    		System.out.println("1");
+                    		System.out.println("11");
                     		break;
                     	case 2:
-                    		System.out.println("2");
+                    		System.out.println("22");
                     		break;
                     	case 3:
-                    		System.out.println("3");
+                    		System.out.println("33");
                     		break;
                     	case 4:
-                    		System.out.println("4");
+                    		System.out.println("44");
                     		break;
                     	case 5:
-                            System.out.println("Saliendo programa total");
+                            System.out.println("Saliendo");
                             break;
        
                     	default:
